f61111434d17c5103d69ca879468f8475328c75b
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-04-14 10:45:01 +0100

    App.js functions and props added

--- todo-list/src/components/App/App.js
+++ todo-list/src/components/App/App.js
@@ -1,8 +1,35 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // useState to create toDoList state - array or string?
+  const [toDoList, setToDoList] = useState([]);
+
+  // function to add an item to the toDoList
+  function addToList() {
+    // add item to toDoList
+    // setToDoList
+    setToDoList = [...toDoList, newToDo];
+  }
+  // function to grab the text input
+  function inputText(event) {
+    setText(event.target.value);
+  }
+
+  // function to remove an item from the toDoList
+  function removeFromList(index) {
+    // remove last from toDoList
+    // setToDoList
+    setToDoList = [...toDoList.slice(0, index), ...toDoList.slice(index + 1)];
+  }
+
   return (
     <div className="App">
+      {/* add Input and List as components*/}
+      {/* Props - add function  */}
+      <Input addToList={addToList} />
+      {/* Props - toDoList (array), delete function */}
+      <List inputText={inputText} removeFromList={removeFromList} />
     </div>
   );
 }
---
+++ todo-list/src/components/Input/Input.js
@@ -0,0 +1,12 @@
+  /*
+<input
+      type="text"
+      onChange={(event) => {
+        handleChange(event);
+      }}
+    /> 
+    
+<button className="randomBootcamperName" onClick={handleBootcamperClick}>
+          Click me for a random bootcamper!
+        </button>
+        */
